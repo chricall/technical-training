f107b97da6d62f1134bd657b65bdfffe05887535
chricall <51904098+chricall@users.noreply.github.com>
2019-06-17 11:53:01 +0000

    =Module 2, some fields playing

--- library/models/rental.py
+++ library/models/rental.py
@@ -5,9 +5,18 @@ from odoo import fields, models
 class Rentals(models.Model):
     _name = 'library.rental'
     _description = 'Book rental'
-
+    
+    #bookname = fields.Char(related="book_id.name",string="Book name")
+    author_ids = fields.Many2many(related="book_id.author_ids",string="Author")
+    edition_date = fields.Date(related="book_id.edition_date", string="Edition Date")
+    
+    customer_email = fields.Char(related="customer_id.email",string="Customer email")
+    customer_address = fields.Text(related="customer_id.address",string="Customer Address")
+    
     customer_id = fields.Many2one('library.partner', string='Customer')
     book_id = fields.Many2one('library.book', string='Book')
 
-    rental_date = fields.Date()
+    rental_date = fields.Date(default=fields.Date.today)
     return_date = fields.Date()
+    
+    
--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models,api
-
+from odoo import fields,models,api
+from odoo.exceptions import ValidationError
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -11,6 +11,8 @@ class Course(models.Model):
 
     responsible_id = fields.Many2one('openacademy.partner', string="Responsible")
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    
+    maxattendees = fields.Integer(string="Maximum Attendees")
 
     level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
 
@@ -25,12 +27,34 @@ class Session(models.Model):
 
     start_date = fields.Date(default=fields.Date.context_today)
     duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
+    
+    maxattendees = fields.Integer(related="course_id.maxattendees",string="Maximum Attendees",store=True)
 
     instructor_id = fields.Many2one('openacademy.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
-    attendees_count = fields.Integer(compute='_compute_attendees_count',string="Count of Attendees")
     
+    # -- method 1 --
+    #@api.constrains('attendee_ids','maxattendees')
+    #def _check_attendees_count(self):
+    #    if self.attendees_count > self.maxattendees:
+    #        raise ValidationError("Your cannot have more than %s records" % self.maxattendees)
+    #
+    #-- method 2 --
+    #_sql_constraints = [
+    #     ('attandeeslimitcheck', 'CHECK (attendees_count <= maxattendees)', 'You have exceed attendees limit'),
+    #]    
+    #
+    @api.onchange('duration')
+    def onchange_duration(self):
+        if self.duration > 5:
+            self.name = self.name+ " (long long course)"
+    
+    attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
+    attendees_count = fields.Integer(compute='_compute_attendees_count',string="Count of Attendees",store=True)
     @api.depends('attendee_ids')
     def _compute_attendees_count(self):
         self.attendees_count = len(self.attendee_ids)
+        
+
+        
+        
