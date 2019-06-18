056755f40b463455d1ab2809547e95616e489a57
chricall <51904098+chricall@users.noreply.github.com>
2019-06-18 07:52:30 +0000

    =inheritence done

--- library/models/book.py
+++ library/models/book.py
@@ -5,7 +5,9 @@ from odoo import fields, models
 class Books(models.Model):
     _name = 'library.book'
     _description = 'Book'
-
+    
+    bookcopy_ids = fields.One2many('library.bookcopy','book_id',string="Book copies")
+    
     name = fields.Char(string='Title')
 
     author_ids = fields.Many2many("library.partner", string="Authors")
@@ -14,3 +16,13 @@ class Books(models.Model):
     publisher_id = fields.Many2one('library.publisher', string='Publisher')
 
     rental_ids = fields.One2many('library.rental', 'book_id', string='Rentals')
+
+class BookCopy(models.Model):
+    _name = "library.bookcopy"
+    _inherits = {'library.book': 'book_id'}
+    _description = "Book copy"
+    
+    book_id = fields.Many2one("library.book",string="Book",required=True,ondelete="cascade")
+    name = fields.Char(string="Copy name")
+    uniqueid = fields.Char(string="Internal Reference")
+    
--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -3,10 +3,11 @@ from odoo import fields, models
 
 
 class Partner(models.Model):
-    _name = 'openacademy.partner'
-    _description = 'Partner'
+    #_name = 'openacademy.partner'
+    #_description = 'Partner'
+    _inherit = "res.partner"
 
-    name = fields.Char()
+    #name = fields.Char()
 
     instructor = fields.Boolean(default=False)
     session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
